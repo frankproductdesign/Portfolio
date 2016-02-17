f1aa1b3418c18f288ca332e58c6eee325ec3cc4d
frank-castro <contact@frankcastro.com>
2016-02-17 16:12:06 -0500

    another file

---
+++ js/controllers/work.controller.js
@@ -0,0 +1,11 @@
+angular.module('myStuff')
+
+	// $scope and $http after bracket protects this function from being messed up with minification, the bracket close at the bottom
+
+.controller('CtrlrTwo', ['$scope', '$http', function ($scope, $http) {
+	var list = this;
+	list.items = []; //empty array so you don't get an error when the page initially loads
+	$http.get('json/work.json').success(function(data) {
+		$scope.items = data;
+	});
+}])
