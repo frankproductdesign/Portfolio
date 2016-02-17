32c99c88bd738a8b360fabaa54dbe2f96388e6a2
frank-castro <contact@frankcastro.com>
2016-02-17 16:10:07 -0500

    work page layout improved

--- js/route-config.js
+++ js/route-config.js
@@ -1,27 +1,30 @@
-angular.module('myApp')
+angular
+	.module('myApp')
 
-.config(['$routeProvider', function($routeProvider) {
+	.config(['$routeProvider', function($routeProvider) {
 
-	$routeProvider.when('/recent', {
-		templateUrl: 'templates/page1/index.html',
-		controller: 'CtrlrOne',
-		controllerAs: 'one'
-	})
-	.when('/previous', { 
-		templateUrl: 'templates/page2/index.html',
-		controller: 'CtrlrTwo',
-		controllerAs: 'two'
-	})
-	.when('/code', { 
-		templateUrl: 'templates/page3/index.html'
-	})
-	.when('/philosophy', { 
-		templateUrl: 'templates/page4/index.html'
-	})
-	.when('/about', { 
-		templateUrl: 'templates/page5/index.html'
-	})
+		$routeProvider
+			.when('/recent', {
+				templateUrl: 'templates/page1/index.html',
+				controller: 'CtrlrOne',
+				controllerAs: 'one'
+			})
+			.when('/previous', { 
+				templateUrl: 'templates/page2/index.html',
+				controller: 'CtrlrTwo',
+				controllerAs: 'two'
+			})
+			.when('/code', { 
+				templateUrl: 'templates/page3/index.html'
+			})
+			.when('/philosophy', { 
+				templateUrl: 'templates/page4/index.html'
+			})
+			.when('/about', { 
+				templateUrl: 'templates/page5/index.html'
+			})
 
-	.otherwise({ redirectTo: '/recent'
-	});
-}]);
+			.otherwise({ redirectTo: '/recent'
+			});
+
+	}]);
