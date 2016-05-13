5ddc64c2022fdaa311ebc23ccb6d3282d66f66da
frank-castro <contact@frankcastro.com>
2016-05-12 22:41:49 -0400

    some tweaks

--- js/directives/menu.dir.js
+++ js/directives/menu.dir.js
@@ -7,3 +7,29 @@ angular
 			templateUrl: "templates/directives/menu.html"
 		};
 	})
+
+	.directive('menuActive', ['$location', function ($location) {
+		return {
+			restrict: 'A',
+			scope: false,
+			link: function (scope, element) {
+				function setActive() {
+					var path = $location.path();
+					if (path) {
+						angular.forEach(element.find('li'), function (li) {
+							var anchor = li.querySelector('a');
+							if (anchor.href.match('#' + path + '(?=\\?|$)')) {
+								angular.element(li).addClass('active');
+							} else {
+								angular.element(li).removeClass('active');
+							}
+						});
+					}
+				}
+
+				setActive();
+
+				scope.$on('$locationChangeSuccess', setActive);
+			}
+		};
+	}]);
