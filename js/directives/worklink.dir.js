a74b302f5a1069aa5d0db1c4b0ae8bf969614a01
frank-castro <contact@frankcastro.com>
2016-11-14 11:12:14 -0500

    last commit before new branch to test submenu in greenspace content

--- js/directives/worklink.dir.js
+++ js/directives/worklink.dir.js
@@ -7,3 +7,10 @@ angular
 			templateUrl: "templates/directives/worklink.html"
 		};
 	})
+
+	// .directive("greenspaceLinks", function() {
+	// 	return {
+	// 		restrict: "E",
+	// 		templateUrl: "templates/directives/worklink.html"
+	// 	};
+	// })
